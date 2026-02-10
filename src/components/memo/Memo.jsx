6de98a05a42d3fe07585222d488e6dda4e4ffa0a
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2026-02-10 16:56:19 +0900

    수정 ui

--- src/components/memo/Memo.jsx
+++ src/components/memo/Memo.jsx
@@ -1,17 +1,24 @@
 import { useMemoStore } from "../../store/useMemoStore";
 import styles from "./memo.module.css";
+import etc_Icon from "../../assets/etc_Icon.svg";
 
 export default function Memo() {
   const memoStore = useMemoStore((state) => state.memos);
+  const formatted = (date) => {
+    return `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
+  };
 
   return (
     <div className={styles.scrollX}>
       <div className={styles.memoBox}>
         {memoStore.map((memo) => (
           <div className={styles.memoWrapper}>
-            <p className={styles.memoSecondTitle}>{memo.object}</p>
+            <div className={styles.memoSecondTitleContainer}>
+              <p className={styles.memoSecondTitle}>{memo.object}</p>
+              <img src={etc_Icon} alt="etc_Icon" />
+            </div>
             <p className={styles.memoContent}>{memo.text}</p>
-            <p className={styles.day}>{memo.date}</p>
+            <p className={styles.day}>{formatted(memo.date)}</p>
           </div>
         ))}
       </div>
