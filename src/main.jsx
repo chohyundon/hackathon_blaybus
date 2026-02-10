d76fba99aa4221b5ab12ef3e31f8c36ae0e2d4ee
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2026-02-10 10:57:08 +0900

    빌드에러 수정

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,11 @@ import Scene from "./components/elements/scene/scene";
 import LearnList from "./learn-list/List";
 import Learn from "./components/learn/Learn";
 import Home from "./components/main/Home";
-import AuthCallback from "./components/auth/AuthCallback";
 
 const router = createBrowserRouter([
   { path: "/scene", element: <Scene /> },
   { path: "/learn-list", element: <LearnList /> },
   { path: "/learn", element: <Learn /> },
-  { path: "/auth/callback", element: <AuthCallback /> },
   { path: "/", element: <Home /> },
 ]);
 
