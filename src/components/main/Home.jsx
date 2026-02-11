8b7ecc8776fdf2f57b01bd672cf53893639a0b9a
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2026-02-11 19:40:43 +0900

    로그아웃 구현

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -114,17 +114,17 @@ AI를 통해 궁금한 내용을 질문해보세요`,
   console.log(user);
 
   const handleLogout = async () => {
-    // const provider = user?.email?.includes("gmail.com") ? "google" : "kakao";
-    // try {
-    //   await fetch(apiUrl(`/auth/withdraw/${provider}`), {
-    //     credentials: "include",
-    //     method: "DELETE",
-    //   });
-    //   setUser(null);
-    // } catch (e) {
-    //   console.warn("logout failed", e);
-    //   setUser(null);
-    // }
+    const provider = user?.email !== null ? "google" : "kakao";
+    try {
+      await fetch(apiUrl(`/auth/withdraw/${provider}`), {
+        credentials: "include",
+        method: "DELETE",
+      });
+      setUser(null);
+    } catch (e) {
+      console.warn("logout failed", e);
+      setUser(null);
+    }
   };
 
   return (
@@ -135,7 +135,7 @@ AI를 통해 궁금한 내용을 질문해보세요`,
           <h1 className={styles.homeTitle}>SIMVEX</h1>
           <p className={styles.homeDescription}>학습 리스트</p>
           {user ? (
-            <div>
+            <div className={styles.homeUserRow}>
               <p
                 className={
                   styles.homeUserName
